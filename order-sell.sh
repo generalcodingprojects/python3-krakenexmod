0e0aec2853e78258008c8513c0b6c9ae1557db15
generalcodingprojects <45795342+generalcodingprojects@users.noreply.github.com>
2018-12-20 14:31:13 +0100

    Update order-sell.sh

--- order-sell.sh
+++ order-sell.sh
@@ -1,1 +1,1 @@
-python3 -c "import api;k=api.API();k.load_key('kraken.key');kraken.query_private('AddOrder', {'pair': 'XXBTZUSD', 'type': 'sell', 'ordertype': 'market', 'volume': 'amountyouwanttosell'})"
+python3 -c "import api;k=api.API();k.load_key('kraken.key');print(k.query_private('AddOrder', {'pair': 'XXBTZUSD', 'type': 'sell', 'ordertype': 'market', 'volume': 'amountyouwanttosell'})"
