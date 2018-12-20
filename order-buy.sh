6cf9524ebf611831fc6654b244b96fd83c49f478
generalcodingprojects <45795342+generalcodingprojects@users.noreply.github.com>
2018-12-20 12:56:47 +0100

    Update order-buy.sh

--- order-buy.sh
+++ order-buy.sh
@@ -1,1 +1,1 @@
-python3 -c "import api;k=api.API();k.load_key('kraken.key');kraken.query_private('AddOrder', {'pair': 'XRPEUR', 'type': 'buy', 'ordertype': 'market', 'volume': '$amountyouwanttobuy'})"
+python3 -c "import api;k=api.API();k.load_key('kraken.key');kraken.query_private('AddOrder', {'pair': 'XXBTZUSD', 'type': 'buy', 'ordertype': 'market', 'volume': 'amountyouwanttobuy'})"
