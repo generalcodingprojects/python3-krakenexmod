233766109ec8c23fec4cf6f8aa7b56b3fcafe919
generalcodingprojects <45795342+generalcodingprojects@users.noreply.github.com>
2019-03-05 12:01:03 +0000

    Update log-open-orders.sh

--- log-open-orders.sh
+++ log-open-orders.sh
@@ -1,1 +1,7 @@
-
+log() {
+  d="logs/$(date "+%Y/%m/%d/%H")"
+  [ ! -d "$d" ] && mkdir -p "$d"
+  echo "$1" >> "$d/$2.txt"
+}
+openorders_all="$(python3 -c "import api;k=api.API();k.load_key('kraken.key');print(k.query_private('OpenOrders'))")"
+log "$openorders_all" "log_balance_allcurrencies"
