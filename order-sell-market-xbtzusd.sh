ee35c6701e1725594a895770ac3744e1b3f12dea
generalcodingprojects <45795342+generalcodingprojects@users.noreply.github.com>
2019-03-05 10:57:44 +0000

    Update and rename order-sell.sh to order-sell-market-xbtzusd.sh

---
+++ order-sell-market-xbtzusd.sh
@@ -0,0 +1,3 @@
+$xbt_amountyouwanttosell='insert_amount_here'
+order_sell -> order-sell-market-xbtzusd.sh
+python3 -c "import api;k=api.API();k.load_key('kraken.key');print(k.query_private('AddOrder', {'pair': 'XXBTZUSD', 'type': 'sell', 'ordertype': 'market', 'volume': 'xbt_amountyouwanttosell'}))"
