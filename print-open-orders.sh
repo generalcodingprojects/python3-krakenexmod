e85378fa0d9812b24773bab22b541a253ad4f25a
generalcodingprojects <45795342+generalcodingprojects@users.noreply.github.com>
2019-03-16 09:54:17 +0000

    Update print-open-orders.sh

--- print-open-orders.sh
+++ print-open-orders.sh
@@ -1,2 +1,2 @@
 openorders_all="$(python3 -c "import api;k=api.API();k.load_key('kraken.key');print(k.query_private('OpenOrders'))")"
-echo "$openorders_all"
+echo "$openorders_all" | tr "'" "\"" | perl -ne '@orders =/"order":\s*"(.*?)"/g; print join("\n", @orders),"\n"'
